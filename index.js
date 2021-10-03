192e1fc450ed57427d0517e7cd75effda189f109
ian <licitdev@gmail.com>
2021-10-04 04:03:04 +0800

    feat: Add loading indicator (#3)
    * Add loading indicator
    
    * feat: Add loading indicator
    
    * fix: add fail spinner when error occured
    
    * fix: prevent possible async issues with loading indicator
    
    * fix: remove stray semi-colon
    
    * style: Fix linting issues
    
    Co-authored-by: Stefan Buck <github@stefanbuck.com>

--- index.js
+++ index.js
@@ -3,16 +3,21 @@
 const getStarredRepos = require('./lib/starred')
 const getTopic = require('./lib/topics')
 const { mapLimit } = require('async')
+const Spinnies = require('spinnies')
 const args = process.argv.slice(2)
 const username = args[0]
 
 main()
 
 async function main () {
-  const starredRepos = await getStarredRepos(username)
+  const spinnies = new Spinnies()
+  const starredRepos = await getStarredRepos(username, spinnies)
+
+  spinnies.add('loading', { text: 'Processing...' })
 
   mapLimit(starredRepos, 20, getTopic, (err, res) => {
     if (err) {
+      spinnies.fail('loading', { text: 'Error occurred!' })
       console.log(err)
       process.exit(1)
     }
@@ -21,8 +26,10 @@ async function main () {
 
     console.log('From %s of your starred repositories %s are participating in Hacktoberfest\n', starredRepos.length, repos.length)
 
-    repos.forEach(repo => {
+    for (const repo of repos) {
       console.log(repo.html_url)
-    })
+    }
+
+    spinnies.succeed('loading', { text: 'Done.' })
   })
 }
